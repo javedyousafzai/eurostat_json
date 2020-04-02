b53087d9c61b3df98f5b4172a982fca9edad31cb
Javed Khan <yousafzaijaved@gmail.com>
2020-04-02 16:53:16 +0200

    changes in index php file

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 <h1>My First Heading</h1>
 <?php
 print " Eurostat Asylum applciations and decisons with API";
+
+print "<p> Annual Asylum applciations</p>";
+print "<p> Monthly Asylum applciations</p>";
+print "<p> Asylum applciations lodged by inaccompanied minors</p>";
+
 ?>
 <p>My first paragraph.</p>
 
