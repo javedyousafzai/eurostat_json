f3ee8e7d806792bf3c789d917043921c970e2d21
Javed Khan <yousafzaijaved@gmail.com>
2020-04-02 16:40:18 +0200

    created index.php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<?php
+print " Eurostat Asylum applciations and decisons with API";
+?>
+<p>My first paragraph.</p>
+
+</body>
+</html>
