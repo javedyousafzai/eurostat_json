dad0ef7b8a2c12ba8c465c674b21f24fa330ef2f
Javed Khan <yousafzaijaved@gmail.com>
2020-04-03 07:37:38 +0200

    changes with template

--- index.php
+++ index.php
@@ -1,17 +1,104 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
-<body>
+	<head>
+		<title>Miniport by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
 
-<h1>My First Heading</h1>
-<?php
-print " Eurostat Asylum applciations and decisons with API";
+		<!-- Nav -->
+		<!-- Nav -->
+			<nav id="nav">
+				<!-- 
+				<ul class="container">
+					<li><a href="#top">Top</a></li>
+					<li><a href="#work">Work</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			-->
+			</nav>
 
-print "<p> Annual Asylum applciations</p>";
-print "<p> Monthly Asylum applciations</p>";
-print "<p> Asylum applciations lodged by inaccompanied minors</p>";
+		<!-- Home -->
+			
 
-?>
-<p>My first paragraph.</p>
+		<!-- Eurostat API Introduction  -->
+			<article id="work" class="wrapper style2">
+				<div class="container">
+					<header>
+						<h2>Eurostat Data - API </h2>
+						<p>These scripts pull data from eurostat API for asylum applications and decisions in the 32 (28 EU+EFTA) countries. Each dataset pull the data and save it in the csv format.</p>
+					</header>
+				<!-- Annual Asylum block -->
+					<div class="row aln-center">
+						<div class="col-4">
+							<section class="box style1">
+								<h3>Asylum and first time asylum applicants by citizenship, age and sex Annual aggregated data</h3>
+								<p>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+							</section>
+						</div>
+				</div>
+				
+				<div><p></p></div>
 
-</body>
+				<!-- Monthly Asylum block -->					
+					<div class="row aln-center">
+						<div class="col-4">
+							<section class="box style1">
+								<h3>First-time asylum applicants by citizenship, age and sex - Monthly data</h3>
+								
+							</section>
+						</div>
+				</div>
+				<div><p></p></div>
+				<!-- Quarterly Decisions -->					
+					<div class="row aln-center">
+						<div class="col-4">
+							<section class="box style1">
+								<h3>	Quarterly decisions</h3>
+								
+							</section>
+						</div>
+				</div>
+
+				<div><p></p></div>
+				<!-- UASC Asylum applications -->					
+					<div class="row aln-center">
+						<div class="col-4">
+							<section class="box style1">
+								<h3>	Asylum applicants considered to be unaccompanied minors by citizenship, age and sex Annual data </h3>
+								
+							</section>
+						</div>
+				</div>
+
+
+			</article>
+
+		<!-- Portfolio -->
+		
+
+		<!-- Contact -->
+			<article id="contact" class="wrapper style4">
+				<div class="container medium">
+					
+					<footer>
+						<ul id="copyright">
+							<li>Here we can provide some footer info if we need</li>
+							
+						</ul>
+					</footer>
+				</div>
+			</article>
+
+		
+
+	</body>
 </html>
